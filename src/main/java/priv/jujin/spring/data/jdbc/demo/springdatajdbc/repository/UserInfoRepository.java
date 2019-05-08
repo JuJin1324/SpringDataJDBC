578f6c8a5d65bc35374ec4573eafdacacd722480
jujin <jujin1324@daum.net>
2019-05-08 15:48:13 +0900

    Where 절 IN 추가

--- src/main/java/priv/jujin/spring/data/jdbc/demo/springdatajdbc/repository/UserInfoRepository.java
+++ src/main/java/priv/jujin/spring/data/jdbc/demo/springdatajdbc/repository/UserInfoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import priv.jujin.spring.data.jdbc.demo.springdatajdbc.entity.UserInfo;
 
+import java.util.List;
+
 /**
  * @author yjj(Yoo Ju Jin)
  * Created 26/04/2019
@@ -25,5 +27,15 @@ public interface UserInfoRepository extends CrudRepository<UserInfo, Long> {
     UserInfo findByUserName(@Param("user_name") String userName);
 
     @Query("SELECT * FROM USER_INFO where id=:id")
-    UserInfo findById(@Param("id") String id);
+    UserInfo findOneById(@Param("id") String id);
+
+    @Query("SELECT " +
+            "USER_INFO.idx AS idx, " +
+            "USER_INFO.id AS id, " +
+            "USER_INFO.password AS password, " +
+            "USER_INFO.user_name AS user_name, " +
+            "USER_INFO.email AS email " +
+            "FROM USER_INFO " +
+            "WHERE USER_INFO.user_name IN (:targetIdList)")
+    List<UserInfo> findAllByIdList(@Param("targetIdList") List<String> targetIdList);
 }
